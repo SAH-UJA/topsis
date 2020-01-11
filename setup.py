6532a98d71b9b818fb05a3e3788c1bf3ed60f4ba
SAH-UJA <sahil27ahuja1999@gmail.com>
2020-01-11 19:06:43 +0530

    Commit 1

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from distutils.core import setup
 setup(
-  name = 'topsis_sahilahuja',         # How you named your package folder (MyLib)
-  packages = ['topsis_sahilahuja'],   # Chose the same as "name"
+  name = 'topsis',         # How you named your package folder (MyLib)
+  packages = ['topsis'],   # Chose the same as "name"
   version = '0.1',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'Topsis analysis of a numeric csv file',   # Give a short description about your library
   author = 'Sahil Ahuja',                   # Type in your name
   author_email = 'sahil27ahuja1999@gmail.com',      # Type in your E-Mail
-  url = 'https://github.com/user/reponame',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/user/reponame/archive/v_01.tar.gz',    # I explain this later on
+  url = 'https://github.com/SAH-UJA/topsis',   # Provide either the link to your github or to your website
+  download_url = 'https://github.com/SAH-UJA/topsis/archive/v_01.tar.gz',    # I explain this later on
   keywords = ['TOPSIS', 'MCDM', 'Statistics'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
           'pandas',
