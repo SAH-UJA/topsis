7d2c9d042dc5e254232f123ce57531b5f0da20a2
SAH-UJA <sahil27ahuja1999@gmail.com>
2020-01-15 20:20:02 +0530

    commit 2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 setup(
   name = 'topsis',         # How you named your package folder (MyLib)
   packages = ['topsis'],   # Chose the same as "name"
-  version = '0.1',      # Start with a small number and increase it with every change you make
+  version = '0.2',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'Topsis analysis of a numeric csv file',   # Give a short description about your library
   author = 'Sahil Ahuja',                   # Type in your name
